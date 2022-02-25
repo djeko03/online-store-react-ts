bf7ed422c4ef3c86cb2040d8582613ff90a595ec
Ruslan <djeko454@mail.ru>
2022-02-25 13:04:12 +0300

    created layout

--- client/src/pages/layout/MainLayout.tsx
+++ client/src/pages/layout/MainLayout.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from "./Header";
+import Footer from "./Footer";
 
-const MainLayout = () => {
+const MainLayout: React.FC = ({children}) => {
     return (
-        <div>
-            
+        <div className='layout'>
+            <Header/>
+            {children}
+            <Footer/>
         </div>
     );
 };
