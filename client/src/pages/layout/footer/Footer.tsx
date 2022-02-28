9f9c7f880b66c4f6b1c44b5ec0fe03ba7e8afc03
Ruslan <djeko454@mail.ru>
2022-02-28 13:54:11 +0300

    temp

--- client/src/pages/layout/footer/Footer.tsx
+++ client/src/pages/layout/footer/Footer.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import FooterLogo from "./FooterLogo/FooterLogo";
+// @ts-ignore
+import css from './Footer.module.css'
+// @ts-ignore
+import container from '../../container/container.module.css'
 
 const Footer = () => {
     return (
-        <section>
-            Footer
+        <section className={css.footer}>
+            <div className={css.content + ' ' + container.container}>
+                <FooterLogo/>
+            </div>
+            <img style={{backgroundColor:'#F9F4E2'}} src="/img/bg.png" alt=""/>
+
         </section>
     );
 };
--- client/src/pages/layout/header/Header.tsx
+++ client/src/pages/layout/header/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Logo from "./Logo/Logo";
 // @ts-ignore
 import css from './Header.module.css'
+// @ts-ignore
+import container from '../../container/container.module.css'
 import CatalogBtn from "./CatalogButton/CatalogBtn";
 import SearchInput from "./SearchInput/SearchInput";
 import Navigation from "./Navigation/Navigation";
@@ -9,15 +11,16 @@ import Profile from "./Profile/Profile";
 
 const Header = () => {
     return (
-        <section className={css.header}>
-            <Logo/>
-            <div className={css.search}>
-                <CatalogBtn/>
-                <SearchInput/>
+        <section>
+            <div className={css.content + ' ' + container.container}>
+                <Logo/>
+                <div className={css.search}>
+                    <CatalogBtn/>
+                    <SearchInput/>
+                </div>
+                <Navigation/>
+                <Profile/>
             </div>
-            <Navigation/>
-            <Profile/>
-
         </section>
     );
 };
