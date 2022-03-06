93636ff7b0fbc14d9118539ef01b1fcf1cb5bbbf
Ruslan <djeko454@mail.ru>
2022-03-06 15:52:36 +0300

    fix(LRS-8):special offers component

--- client/src/component/special-offers/index.tsx
+++ client/src/component/special-offers/index.tsx
@@ -9,10 +9,18 @@ export const SpecialOffers = () => {
             <Title>Специальные предложения</Title>
             <div className={scss.banners}>
                 <div className={scss.orange}>
-                    <img src="img/special-offers-img/banner.png" alt=""/>
+                    <div className={scss.content}>
+                        <p className={scss.title}>Оформите карту «Северяночка»</p>
+                        <p className={scss.text}>И получайте бонусы при покупке в магазинах и на сайте</p>
+                    </div>
+                    <img className={scss.image} src="img/special-offers-img/orangeSection.jpg" alt=""/>
                 </div>
                 <div className={scss.green}>
-                    <img src="img/special-offers-img/banner (1).png" alt=""/>
+                    <div className={scss.content}>
+                        <p className={scss.title}>Покупайте акционные товары</p>
+                        <p className={scss.text}>И получайте вдвое больше бонусов</p>
+                    </div>
+                    <img className={scss.image} src="img/special-offers-img/greenSection.jpg" alt=""/>
                 </div>
             </div>
         </div>
