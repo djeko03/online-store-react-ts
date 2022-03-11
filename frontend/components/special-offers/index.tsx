758a953c95a19fda73ed3eeb5344d3f41aca4bb6
Ruslan <djeko454@mail.ru>
2022-03-11 11:44:58 +0300

    fix(LRS-13):img component

--- frontend/components/special-offers/index.tsx
+++ frontend/components/special-offers/index.tsx
@@ -11,14 +11,14 @@ export const SpecialOffers = () => {
                         <p className={scss.title}>Оформите карту «Северяночка»</p>
                         <p className={scss.text}>И получайте бонусы при покупке в магазинах и на сайте</p>
                     </div>
-                    <MyImage layout={MyLayout.intrinsic} width={584} height={200} src={'/img/special-offers-img/orangeSection.jpg'}/>
+                    <MyImage layout={MyLayout.responsive} width={584} height={200} src={'/img/special-offers-img/orangeSection.jpg'}/>
                 </div>
                 <div className={scss.green}>
                     <div className={scss.content}>
                         <p className={scss.title}>Покупайте акционные товары</p>
                         <p className={scss.text}>И получайте вдвое больше бонусов</p>
                     </div>
-                    <MyImage layout={MyLayout.intrinsic} width={584} height={200} src={'/img/special-offers-img/greenSection.jpg'}/>
+                    <MyImage layout={MyLayout.responsive} width={584} height={200} src={'/img/special-offers-img/greenSection.jpg'}/>
                 </div>
             </div>
         </div>
